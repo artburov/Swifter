9e4a14172deba0d16f08c71f02749c137d0dfa54
Arthur Burov <art.burov@gmail.com>
2020-04-12 16:12:28 +0300

    init

---
+++ HomeWork6/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  HomeWork6
+//
+//  Created by Admin on 12.04.2020.
+//  Copyright © 2020 Khan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        /*
+         init
+         */
+    }
+
+
+}
+
